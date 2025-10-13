6f8fbdb6ec87a86334d48da2af398c74a97bcf2d
projetoPorgamacao <ricardinhofl68@gmail.com>
2025-10-13 17:26:24 -0300

    Primeiro commit

---
+++ src/main/java/com/example/ProjetoProgamacao2/ProjetoProgamacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoProgamacao2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoProgamacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoProgamacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjetoProgamacao2/ProjetoProgamacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoProgamacao2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoProgamacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
